49b0ccaf7768f8def2d2975699d60756745bf85c
André Cristen <30880513+andrecristen@users.noreply.github.com>
2024-07-03 11:11:42 -0300

    Update docusaurus.config.js

--- docusaurus.config.js
+++ docusaurus.config.js
@@ -1,12 +1,31 @@
 module.exports = {
-    title: 'All Services',
-    tagline: 'Documentação do All Services',
-    url: 'https://andrecristen.github.io',
-    baseUrl: '/all-services/',
-    onBrokenLinks: 'throw',
-    onBrokenMarkdownLinks: 'warn',
-    favicon: 'img/favicon.ico',
-    organizationName: 'andrecristen',
-    projectName: 'all-services',
-    trailingSlash: false,
-};  
+  title: 'Seu Título',
+  tagline: 'Seu Tagline',
+  url: 'https://andrecristen.github.io', // URL do seu site
+  baseUrl: '/all-services/', // Caminho base
+  onBrokenLinks: 'throw',
+  onBrokenMarkdownLinks: 'warn',
+  favicon: 'img/favicon.ico',
+  organizationName: 'andrecristen', // Nome do seu usuário ou organização no GitHub
+  projectName: 'all-services', // Nome do seu repositório no GitHub
+  deploymentBranch: 'gh-pages', // Branch usada para o GitHub Pages
+  trailingSlash: false, // ou true, dependendo da sua preferência e configuração
+  presets: [
+    [
+      '@docusaurus/preset-classic',
+      {
+        docs: {
+          sidebarPath: require.resolve('./sidebars.js'),
+          editUrl: 'https://github.com/andrecristen/all-services/edit/main/website/',
+        },
+        blog: {
+          showReadingTime: true,
+          editUrl: 'https://github.com/andrecristen/all-services/edit/main/website/blog/',
+        },
+        theme: {
+          customCss: require.resolve('./src/css/custom.css'),
+        },
+      },
+    ],
+  ],
+};
