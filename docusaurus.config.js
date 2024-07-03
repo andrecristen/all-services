58283b5ea4df1efb59588424c974803f9d0dc6a9
André Cristen <30880513+andrecristen@users.noreply.github.com>
2024-07-02 22:04:23 -0300

    docs

--- docusaurus.config.js
+++ docusaurus.config.js
@@ -1,4 +1,6 @@
 module.exports = {
+    title: 'All Services',
+    tagline: 'Documentação do All Services',
     url: 'https://andrecristen.github.io',
     baseUrl: '/all-services/',
     projectName: 'all-services',
