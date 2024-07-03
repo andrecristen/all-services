53ff51b174a7f858b345f17a5fc33707af2fd2f0
André Cristen <30880513+andrecristen@users.noreply.github.com>
2024-07-02 21:57:23 -0300

    website

---
+++ docusaurus.config.js
@@ -0,0 +1,8 @@
+module.exports = {
+    url: 'https://andrecristen.github.io',
+    baseUrl: '/all-services/',
+    projectName: 'all-services',
+    organizationName: 'andrecristen',
+    trailingSlash: false,
+  };
+  
